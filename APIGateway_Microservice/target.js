5be4c1b5a2cbc6ea66dfc7194e240b772883f615
poVvisal <156397412+poVvisal@users.noreply.github.com>
2025-06-19 16:58:39 +0700

    moded compose

--- APIGateway_Microservice/target.js
+++ APIGateway_Microservice/target.js
@@ -1,5 +1,5 @@
 module.exports = {
-    REGISTRATION_SERVICE: 'http://registration:5001',
-    COACH_SERVICE: 'http://coach:5002', 
-    PLAYER_SERVICE: 'http://player:5003'
+    REGISTRATION_SERVICE: process.env.REGISTRATION_SERVICE_URL || 'http://registration:5001',
+    COACH_SERVICE: process.env.COACH_SERVICE_URL || 'http://coach:5002', 
+    PLAYER_SERVICE: process.env.PLAYER_SERVICE_URL || 'http://player:5003'
 };
